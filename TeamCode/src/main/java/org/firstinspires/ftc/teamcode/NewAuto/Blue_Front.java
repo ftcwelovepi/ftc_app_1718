cffa74ccc657fc6bbad36fab2b54ec8df3a60291
SwagsterWagster <mrinaalr@gmail.com>
2018-02-16 21:56:02 -0800

    image reg on Blue Right.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAuto/Blue_Front.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAuto/Blue_Front.java
@@ -6,13 +6,13 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
  * Created by Swagster_Wagster on 2/5/18.
  */
 
-@Autonomous(name= "Blue_Front", group = "We Love PI")
+@Autonomous(name= "Blue_Right", group = "We Love PI")
 public class Blue_Front extends Auto {
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        myRunOpMode(.5, 15, ColorSensor.ColorName.BLUE, true ,0.5, 1800, 85,0, 0,0,-0.3,1100,0.3, 1000);
+        myRunOpMode(.5, 15, ColorSensor.ColorName.BLUE, true , true, 0.5, 1800, 85,0, 0,0,-0.3,1100,0.3, 1000);
 
     }
 }
