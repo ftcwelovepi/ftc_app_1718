e9dc09ca68c4a4d5a1759722b8b33912c06722c5
Roma Bhatia (@sugarcrystals01) <sugarcrystals01@gmail.com>
2018-02-17 14:21:55 -0800

    d_pad limiters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/WLP_MecanumDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/WLP_MecanumDriveTrain.java
@@ -45,8 +45,8 @@ import org.firstinspires.ftc.teamcode.Autonomous.WLP_RR_JewelArm;
 
 public class WLP_MecanumDriveTrain {
 
-    final public double max_stick = 1.0;
-    final public double min_stick = -1.0;
+    public double max_stick = 1.0;
+    public double min_stick = -1.0;
 
     // Declare Drive Train modors
     protected DcMotor frontLeft = null;
@@ -114,7 +114,7 @@ public class WLP_MecanumDriveTrain {
         double left_x = 0.0;
         double left_y = 0.0;
         double right_x = 0.0;
-        double left_toggle = 0.3;
+        double left_toggle = 0.4;
         double right_toggle = 0.5;
         double gamepadSum = gamepad1.left_stick_x + gamepad1.left_stick_y + gamepad1.right_stick_x;
 
@@ -132,10 +132,14 @@ public class WLP_MecanumDriveTrain {
             left_x = gamepad1.left_stick_x * left_toggle;
             left_y = gamepad1.left_stick_y * left_toggle;
             right_x = gamepad1.right_stick_x * left_toggle;
+            max_stick = max_stick * left_toggle;
+            min_stick = min_stick * left_toggle;
         }else if(gamepad1.right_trigger > 0){
             left_x = gamepad1.left_stick_x * right_toggle;
             left_y = gamepad1.left_stick_y * right_toggle;
             right_x = gamepad1.right_stick_x * right_toggle;
+            max_stick = max_stick * right_toggle;
+            min_stick = min_stick * right_toggle;
         } else {
             left_x = gamepad1.left_stick_x;
             left_y = gamepad1.left_stick_y;
